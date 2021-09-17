f78482b443dafdbadf11338fa88570b9511dda28
freek <freek@spatie.be>
2021-09-17 12:44:30 +0200

    wip

--- src/Drivers/MeiliSearchDriver.php
+++ src/Drivers/MeiliSearchDriver.php
@@ -44,7 +44,7 @@ class MeiliSearchDriver implements Driver
         $rawResults = $this->index()->rawSearch($query, [
             'attributesToHighlight' => ['entry', 'description'],
         ]);
-        ray($rawResults);
+
         $hits = array_map(function (array $hitProperties) {
             return new Hit(
                 $hitProperties['id'],

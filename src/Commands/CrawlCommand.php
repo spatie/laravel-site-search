a66947651b273cc926c36e765a0d7d434c54e2b0
Alex Vanderbist <alex.vanderbist@gmail.com>
2025-06-18 16:17:14 +0200

    Add sync option to crawlcommand

--- src/Commands/CrawlCommand.php
+++ src/Commands/CrawlCommand.php
@@ -7,17 +7,22 @@ use Spatie\SiteSearch\Models\SiteSearchConfig;
 
 class CrawlCommand extends Command
 {
-    public $signature = 'site-search:crawl';
+    public $signature = 'site-search:crawl {--sync}';
 
     public function handle()
     {
+        $sync = $this->option('sync');
         $crawlSiteJob = config('site-search.crawl_site_job');
 
         SiteSearchConfig::enabled()
-            ->each(function (SiteSearchConfig $siteSearchConfig) use ($crawlSiteJob) {
+            ->each(function (SiteSearchConfig $siteSearchConfig) use ($sync, $crawlSiteJob) {
                 $this->comment("Dispatching job to crawl `{$siteSearchConfig->crawl_url}`");
 
-                dispatch(new $crawlSiteJob($siteSearchConfig));
+                if ($sync) {
+                    dispatch_sync(new $crawlSiteJob($siteSearchConfig));
+                } else {
+                    dispatch(new $crawlSiteJob($siteSearchConfig));
+                }
             });
 
         $this->info('All done');
